7e650209dc03b6579d2e01ec8bf1cdfa611dc2c6
David Morris <davidrowlandmorris@gmail.com>
2011-11-04 15:38:07 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheYorker::Application.config.secret_token = '10e683c90506cb708a2c85d313e381c7c038f4e552cfa940bad5e6b0d18b5cb36ceee2697d0e5601a8ac2eff16d8ae8a45a68550e1b6640b9e5075f9652c76d6'
