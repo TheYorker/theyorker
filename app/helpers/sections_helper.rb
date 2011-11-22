766c104a0450d7de79f4faee93c6e43e10fafb51
David Morris <davidrowlandmorris@gmail.com>
2011-11-22 21:02:07 +0000

    Dynamic Toplevel Tabs

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -12,4 +12,11 @@ module SectionsHelper
     # paths.sort! {|a,b| a.first.last <=> b.first.last}
   end
   
+  def toplevel_sections_list
+    toplevel_sections = Section.find_all_by_parent_id(1);
+    toplevel_sections.map do |s|
+      ["<li>", link_to(s.name, section_path(s)), "</li>"].join
+    end.join.html_safe
+  end
+  
 end
