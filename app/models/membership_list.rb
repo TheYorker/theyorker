9fd97062d07d3b5892a8d843e8562f868e324424
David Morris <davidrowlandmorris@gmail.com>
2011-11-24 17:37:37 +0000

    Membership validation

--- app/models/membership_list.rb
+++ app/models/membership_list.rb
@@ -1,2 +1,14 @@
 class MembershipList < ActiveRecord::Base
+
+  def self.current_members
+    lists = MembershipList.where(['? BETWEEN start_date AND end_date', Time.now.to_date])
+    lists.map do |l|
+      l.addresses
+    end.join("\r\n").split("\r\n").uniq
+  end
+
+  def self.member?(address)
+    MembershipList.current_members.include? address
+  end
+  
 end
