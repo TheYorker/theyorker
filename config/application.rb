201694082eb5dd2404a9d03ecca9ae07ec0b67a5
David Morris <davidrowlandmorris@gmail.com>
2011-11-30 20:56:08 +0000

    Fix assets

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,8 @@ module TheYorker
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile += ['member.css']
+
   end
 end
