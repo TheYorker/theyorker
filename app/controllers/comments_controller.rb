0589261bdd450127b9484efa8237ca0994c22a96
David Morris <davidrowlandmorris@gmail.com>
2011-12-09 11:32:38 +0000

    Associate comment with user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,15 @@ class CommentsController < ApplicationController
   before_filter :admin_access, :only => ['hide', 'unhide']
 
   def create
+    if !current_user
+      flash.now.error = "Must be logged in to post comments"
+      redirect_to article_path(article)
+      return
+    end
     article = Article.find(params[:id])
     comment = article.comments.create(params[:comment])
+    comment.user = current_user
+    comment.save
     redirect_to article_path(article)
   end
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,7 @@
 class Comment < ActiveRecord::Base
 
   belongs_to :article
-
-  def check_byline
-    if self.byline.empty?
-      self.byline = "Anonymous"
-    end
-  end
+  belongs_to :user
 
   def hide
     self.hidden = true
