66dfa9cdcdebd6b0ef1471af9ff639eb9a0c10bb
David Morris <davidrowlandmorris@gmail.com>
2012-01-04 19:37:58 +0000

    Merge changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+unless Section.exists?(1)
+  Section.create(name: 'The Yorker')
+end
