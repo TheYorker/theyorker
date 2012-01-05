089aa3a092b32fb93209893547ec64b30543fed7
David Morris <davidrowlandmorris@gmail.com>
2012-01-05 19:38:58 +0000

    Add useful seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,45 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+puts "Checking sections...\n"
 unless Section.exists?(1)
+  puts "Creating section with id=1\n"
   Section.create(name: 'The Yorker')
 end
 
+
+puts "Checking privilege lists...\n"
 unless PrivilegeList.exists?(1)
+  puts "Creating initial privilege list\n"
   PrivilegeList.create(level: 2,
-                       addresses: 'webmaster@theyorker.co.uk',
+                       addresses: ['webmaster@theyorker.co.uk',
+                                   'editor@theyorker.co.uk',
+                                   'director@theyorker.co.uk'].join("\r\n"),
                        start_date: Time.now - 5.years,
                        end_date: Time.now + 100.years)
+  
 end
 
-unless User.exists?(1)
+puts "Checking users...\n"
+unless User.where(:email => 'webmaster@theyorker.co.uk').exists?
+  puts "Creating webmaster@theyorker.co.uk\n"
   User.create(name: 'Webmaster',
               email: 'webmaster@theyorker.co.uk',
               password: 'changeme',
               password_confirmation: 'changeme')
+end
+
+unless User.where(:email => 'editor@theyorker.co.uk').exists?
+  puts "Creating editor@theyorker.co.uk"
+  User.create(name: 'Editor',
+              email: 'editor@theyorker.co.uk',
+              password: 'changeme',
+              password_confirmation: 'changeme')
+end
+
+puts "Checking editor privileges...\n"
+unless User.find_by_email('editor@theyorker.co.uk').editor_for(Section.find(1))
+  puts "Assigning global editor privileges to editor@theyorker.co.uk\n"
+  Editor.create(section: Section.find(1),
+                user: User.find_by_email('editor@theyorker.co.uk'))
 end
