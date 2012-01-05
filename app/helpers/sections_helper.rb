39b98eae1507034d160a0c92c1bd0eb3a12c0653
David Morris <davidrowlandmorris@gmail.com>
2012-01-05 18:22:20 +0000

    Correct typo in section helper

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -5,7 +5,7 @@ module SectionsHelper
     section_paths = sections.map(&:fullpath)
   end
 
-  def all_sections_as_text_paths_with_idb
+  def all_sections_as_text_paths_with_id
     paths = all_sections_as_text_paths.map do |p|
       [p.map(&:name).join('/'), p.last.id]
     end
