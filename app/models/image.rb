ba6cdef22f951fa95b2b52c16d25f1b54dd15f75
David Morris <davidrowlandmorris@gmail.com>
2012-01-06 06:10:46 +0000

    Improve image search

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,7 @@
 class Image < ActiveRecord::Base
   has_attached_file :picture, :styles => {:medium => '300x300',
-                                          :large_thumb => '150x150'}
+                                          :large_thumb => '150x150',
+                                          :small_thumb => '50x50'}
 
   searchable do
     text :title, :description, :tags
