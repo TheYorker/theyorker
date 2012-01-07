320bcbfd7d0f87d0e14dacfc7addc9f7d76a6aca
David Morris <davidrowlandmorris@gmail.com>
2012-01-07 16:57:29 +0000

    Make capistrano play nice with assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,6 +16,8 @@ $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory
 require "rvm/capistrano"                  # Load RVM's capistrano plugin.
 set :rvm_ruby_string, "ruby-1.9.2@#{user}"        # Or whatever env you want it to run in.
 
+# assets
+load 'deploy/assets'
 
 # bundler
 
