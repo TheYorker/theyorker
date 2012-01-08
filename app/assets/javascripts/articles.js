65e3ca89efe8e71c2e6d214bec9465cda33bbebf
David Morris <davidrowlandmorris@gmail.com>
2012-01-08 16:27:39 +0000

    Fix width of articles with sidebar

---
+++ app/assets/javascripts/articles.js
@@ -0,0 +1,13 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+// correct_size = function(container) {
+//     $(container).css('width', $('img', container).width().toString());
+// };
+
+// $(window).load(function () {
+//     image_containers = $('.main_image');
+//     for (c in image_containers) {
+//         correct_size(c);
+//     };
+// });
