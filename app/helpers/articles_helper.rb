ef95f0427ac767730b76e8d78c86437203d5a84b
David Morris <davidrowlandmorris@gmail.com>
2012-01-18 14:33:58 +0000

    Move markdown rendering from model to helper

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -19,5 +19,47 @@ module ArticlesHelper
       "Don't understand this visibility! Please file a bug report"
     end
   end
+
+  def render_sanitized_markdown(input)
+    config = Sanitize::Config::RELAXED
+    youtube_transformer = lambda do |env|
+      node      = env[:node]
+      node_name = env[:node_name]
+
+      # Don't continue if this node is already whitelisted or is not an element.
+      return if env[:is_whitelisted] || !node.element?
+      
+      # Don't continue unless the node is an iframe.
+      return unless node_name == 'iframe'
+      
+      # Verify that the video URL is actually a valid YouTube video URL.
+      return unless node['src'] =~ /\Ahttps?:\/\/(?:www\.)?youtube(?:-nocookie)?\.com\//
+      
+      # We're now certain that this is a YouTube embed, but we still need to run
+      # it through a special Sanitize step to ensure that no unwanted elements or
+      # attributes that don't belong in a YouTube embed can sneak in.
+      Sanitize.clean_node!(node, {
+                             :elements => %w[iframe],
+                             
+                             :attributes => {
+                               'iframe'  => %w[allowfullscreen frameborder height src width]
+                             }
+                           })
+      
+      # Now that we're sure that this is a valid YouTube embed and that there are
+      # no unwanted elements or attributes hidden inside it, we can tell Sanitize
+      # to whitelist the current node.
+      {:node_whitelist => [node]}
+    end
+
+    config[:transformers] = [youtube_transformer]
+    config[:elements] += ['div']
+    config[:attributes][:all] += ['class']
+
+    html = Sanitize.clean(BlueCloth.new(input).to_html, config)
+    # increase heading levels of markdown output by 2
+    html.gsub(/<(\/?)h([0-7])>/) {"<#$1h#{$2.to_i+2}>"}.html_safe
+  end
+
   
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -95,47 +95,6 @@ class Article < ActiveRecord::Base
     self.featuredness - age_in_days
   end
 
-  def render
-    config = Sanitize::Config::RELAXED
-    youtube_transformer = lambda do |env|
-      node      = env[:node]
-      node_name = env[:node_name]
-
-      # Don't continue if this node is already whitelisted or is not an element.
-      return if env[:is_whitelisted] || !node.element?
-      
-      # Don't continue unless the node is an iframe.
-      return unless node_name == 'iframe'
-      
-      # Verify that the video URL is actually a valid YouTube video URL.
-      return unless node['src'] =~ /\Ahttps?:\/\/(?:www\.)?youtube(?:-nocookie)?\.com\//
-      
-      # We're now certain that this is a YouTube embed, but we still need to run
-      # it through a special Sanitize step to ensure that no unwanted elements or
-      # attributes that don't belong in a YouTube embed can sneak in.
-      Sanitize.clean_node!(node, {
-                             :elements => %w[iframe],
-                             
-                             :attributes => {
-                               'iframe'  => %w[allowfullscreen frameborder height src width]
-                             }
-                           })
-      
-      # Now that we're sure that this is a valid YouTube embed and that there are
-      # no unwanted elements or attributes hidden inside it, we can tell Sanitize
-      # to whitelist the current node.
-      {:node_whitelist => [node]}
-    end
-
-    config[:transformers] = [youtube_transformer]
-    config[:elements] += ['div']
-    config[:attributes][:all] += ['class']
-
-    html = Sanitize.clean(BlueCloth.new(self.body).to_html, config)
-    # increase heading levels of markdown output by 2
-    html.gsub(/<(\/?)h([0-7])>/) {"<#$1h#{$2.to_i+2}>"}.html_safe
-  end
-
   def headline_image
     self.image ? self.image.picture.url(:headline) : 'ylogo_headline.png'
   end
