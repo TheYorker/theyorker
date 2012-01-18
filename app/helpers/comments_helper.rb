0fe386ed576cda290c7b80808544de0f01d10d08
David Morris <davidrowlandmorris@gmail.com>
2012-01-18 16:38:18 +0000

    Factor commenter names out of _comment partial

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -3,5 +3,21 @@ module CommentsHelper
   def article_comments_path(article)
     "/articles/#{article.id}/comments"
   end
-  
+
+  def comment_user(comment)
+    if comment.anonymous?
+      "Anonymous"
+    else
+      link_to comment.user.name, user_path(comment.user)
+    end
+  end
+
+  def anonymous_comment_user(comment)
+    if is_admin? && comment.anonymous
+      link_to "[#{comment.user.name}]", user_path(comment.user)
+    else
+      ""
+    end
+  end
+
 end
