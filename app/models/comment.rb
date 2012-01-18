60fd7a10c0b81f1b8e344d956ed318987dc1123e
David Morris <davidrowlandmorris@gmail.com>
2012-01-18 16:37:59 +0000

    Add recent method to Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,10 @@ class Comment < ActiveRecord::Base
   belongs_to :article
   belongs_to :user
 
+  def self.recent(limit=5)
+    Comment.order('created_at DESC').where("hidden = ? OR ?", false, nil).limit(limit)
+  end
+
   def hide
     self.hidden = true
     self.save
