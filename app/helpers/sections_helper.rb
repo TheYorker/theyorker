c7738887dbb1779e98fa3bae14c1e07ab03b7497
David Morris <davidrowlandmorris@gmail.com>
2012-01-20 02:23:08 +0000

    Only allow creating articles in leaf sections

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -5,13 +5,24 @@ module SectionsHelper
     section_paths = sections.map(&:fullpath)
   end
 
+  def leaf_sections_as_text_paths
+    sections = Section.all
+    sections.find_all {|s| s.children == []}.map(&:fullpath)
+  end
+
   def all_sections_as_text_paths_with_id
     paths = all_sections_as_text_paths.map do |p|
       [p.map(&:name).join('/'), p.last.id]
     end
     # paths.sort! {|a,b| a.first.last <=> b.first.last}
   end
-  
+
+  def leaf_sections_as_text_paths_with_id
+    paths = leaf_sections_as_text_paths.map do |p|
+      [p.map(&:name).join('/'), p.last.id]
+    end
+  end
+
   def toplevel_sections_list
     toplevel_sections = Section.find_all_by_parent_id(1);
     toplevel_sections.map do |s|
