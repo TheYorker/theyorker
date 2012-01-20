c54f364c8a0de27ba54bae763b69ffab4267ab99
David Morris <davidrowlandmorris@gmail.com>
2012-01-20 00:44:39 +0000

    image_with_caption should not class enclosing div as main_image if image size is :original

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -4,7 +4,7 @@ module ImagesHelper
     image = Image.find(id)
     width = image.picture.width(size)
     embed = ERB.new <<EOF
-<div class="main_image" width="<%= width %>"><img src="<%= image.picture.url(size) %>" / ><div class="image_caption" style="max-width:<%= image.picture.width(size) %>px;"><%= caption %> &copy;<%= image.copyright_owner %><%= image.image_credit != "" ? "; Image Credit: #{image.image_credit}" : "" %></div></div>
+<div <%= size == :medium ? 'class="main_image"' : '' %>><img src="<%= image.picture.url(size) %>" / ><div class="image_caption" style="max-width:<%= image.picture.width(size) %>px;"><%= caption %> &copy;<%= image.copyright_owner %><%= image.image_credit != "" ? "; Image Credit: #{image.image_credit}" : "" %></div></div>
 EOF
     embed.result(binding)
   end
