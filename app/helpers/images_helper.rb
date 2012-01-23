1f5ab24bf2a765c55fe7e82a3459752971e25e5d
David Morris <davidrowlandmorris@gmail.com>
2012-01-23 20:48:58 +0000

    Force unicode encoding for image width in image_with_caption

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -2,9 +2,11 @@ module ImagesHelper
   
   def image_with_caption(id, caption, size=:medium)
     image = Image.find(id)
-    width = image.picture.width(size)
+    width = "#{image.picture.width(size)}".force_encoding('UTF-8')
+    credit = "#{image.image_credit}".force_encoding('UTF-8')
+    
     embed = ERB.new <<EOF
-<div <%= size == :medium ? 'class="main_image"' : 'class="image_container"' %>><img src="<%= image.picture.url(size) %>" / ><div class="image_caption" style="max-width:<%= image.picture.width(size) %>px;"><%= caption %> &copy;<%= image.copyright_owner %><%= image.image_credit != "" ? "; Image Credit: #{image.image_credit}" : "" %></div></div>
+<div <%= size == :medium ? 'class="main_image"' : 'class="image_container"' %>><img src="<%= image.picture.url(size) %>" / ><div class="image_caption" style="max-width:#{width}px;"><%= caption %> &copy;<%= image.copyright_owner %><%= image.image_credit != "" ? "; Image Credit: #{image.image_credit}" : "" %></div></div>
 EOF
     embed.result(binding)
   end
