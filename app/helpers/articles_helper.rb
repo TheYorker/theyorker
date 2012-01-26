56cb6f036a6df4f3978d2d4894080f4ea6fb44cf
David Morris <davidrowlandmorris@gmail.com>
2012-01-26 11:22:00 +0000

    Replace force_encoding with encode (oops)

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -18,7 +18,7 @@ module ArticlesHelper
       return ""
     end
 
-    input.force_encoding('UTF-8')
+    input.encode!('UTF-8')
 
     config = Sanitize::Config::RELAXED
     youtube_transformer = lambda do |env|
--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -2,13 +2,13 @@ module ImagesHelper
   
   def image_with_caption(id, caption, size=:medium)
     image = Image.find(id)
-    width = "#{image.picture.width(size)}".force_encoding('UTF-8')
-    credit = "#{image.image_credit}".force_encoding('UTF-8')
+    width = "#{image.picture.width(size)}".encode('UTF-8')
+    credit = "#{image.image_credit}".encode('UTF-8')
     
     embed = ERB.new <<EOF
 <div <%= size == :medium ? 'class="main_image"' : 'class="image_container"' %>><img src="<%= image.picture.url(size) %>" / ><div class="image_caption" style="max-width:#{width}px;"><%= caption %> &copy;<%= image.copyright_owner %><%= image.image_credit != "" ? "; Image Credit: #{image.image_credit}" : "" %></div></div>
 EOF
-    embed.result(binding).force_encoding('UTF-8')
+    embed.result(binding).encode('UTF-8')
   end
 
 
