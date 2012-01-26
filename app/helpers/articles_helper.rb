e8453e3086b5a9aebe726077bfab54b6aee34e5b
David Morris <davidrowlandmorris@gmail.com>
2012-01-26 11:22:52 +0000

    Trap exceptions in render_sanitized_markdown

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -14,53 +14,59 @@ module ArticlesHelper
   end
 
   def render_sanitized_markdown(input)
-    if !input
-      return ""
+    begin
+      if !input
+        return ""
+      end
+
+      input.encode!('UTF-8')
+
+      config = Sanitize::Config::RELAXED
+      youtube_transformer = lambda do |env|
+        node      = env[:node]
+        node_name = env[:node_name]
+
+        # Don't continue if this node is already whitelisted or is not an element.
+        return if env[:is_whitelisted] || !node.element?
+        
+        # Don't continue unless the node is an iframe.
+        return unless node_name == 'iframe'
+        
+        # Verify that the video URL is actually a valid YouTube video URL.
+        return unless node['src'] =~ /\Ahttps?:\/\/(?:www\.)?youtube(?:-nocookie)?\.com\//
+        
+        # We're now certain that this is a YouTube embed, but we still need to run
+        # it through a special Sanitize step to ensure that no unwanted elements or
+        # attributes that don't belong in a YouTube embed can sneak in.
+        Sanitize.clean_node!(node, {
+                               :elements => %w[iframe],
+                               
+                               :attributes => {
+                                 'iframe'  => %w[allowfullscreen frameborder height src width]
+                               }
+                             })
+        
+        # Now that we're sure that this is a valid YouTube embed and that there are
+        # no unwanted elements or attributes hidden inside it, we can tell Sanitize
+        # to whitelist the current node.
+        {:node_whitelist => [node]}
+      end
+
+      config[:transformers] = [youtube_transformer]
+      config[:elements] += ['div']
+      config[:attributes][:all] += ['class']
+      config[:attributes]['div'] = ['style']
+
+      template = ERB.new(input)
+
+      html = Sanitize.clean(BlueCloth.new(template.result(binding)).to_html, config)
+      # increase heading levels of markdown output by 2
+      html.gsub(/<(\/?)h([0-7])>/) {"<#$1h#{$2.to_i+2}>"}.html_safe
+      
+    rescue
+      "Unable to render page"
     end
 
-    input.encode!('UTF-8')
-
-    config = Sanitize::Config::RELAXED
-    youtube_transformer = lambda do |env|
-      node      = env[:node]
-      node_name = env[:node_name]
-
-      # Don't continue if this node is already whitelisted or is not an element.
-      return if env[:is_whitelisted] || !node.element?
-      
-      # Don't continue unless the node is an iframe.
-      return unless node_name == 'iframe'
-      
-      # Verify that the video URL is actually a valid YouTube video URL.
-      return unless node['src'] =~ /\Ahttps?:\/\/(?:www\.)?youtube(?:-nocookie)?\.com\//
-      
-      # We're now certain that this is a YouTube embed, but we still need to run
-      # it through a special Sanitize step to ensure that no unwanted elements or
-      # attributes that don't belong in a YouTube embed can sneak in.
-      Sanitize.clean_node!(node, {
-                             :elements => %w[iframe],
-                             
-                             :attributes => {
-                               'iframe'  => %w[allowfullscreen frameborder height src width]
-                             }
-                           })
-      
-      # Now that we're sure that this is a valid YouTube embed and that there are
-      # no unwanted elements or attributes hidden inside it, we can tell Sanitize
-      # to whitelist the current node.
-      {:node_whitelist => [node]}
-    end
-
-    config[:transformers] = [youtube_transformer]
-    config[:elements] += ['div']
-    config[:attributes][:all] += ['class']
-    config[:attributes]['div'] = ['style']
-
-    template = ERB.new(input)
-
-    html = Sanitize.clean(BlueCloth.new(template.result(binding)).to_html, config)
-    # increase heading levels of markdown output by 2
-    html.gsub(/<(\/?)h([0-7])>/) {"<#$1h#{$2.to_i+2}>"}.html_safe
   end
 
   
