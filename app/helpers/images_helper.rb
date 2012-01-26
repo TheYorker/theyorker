b5aae1673f40c7ae958bc59af7795ac38ab7c0d9
David Morris <davidrowlandmorris@gmail.com>
2012-01-26 22:34:57 +0000

    A little further on encoding :(

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -6,7 +6,7 @@ module ImagesHelper
     credit = ensure_utf8("#{image.image_credit}")
     
     template = <<EOF
-<div <%= size == :medium ? 'class="main_image"' : 'class="image_container"' %>><img src="<%= image.picture.url(size) %>" / ><div class="image_caption" style="max-width:#{width}px;"><%= caption %> &copy;<%= image.copyright_owner %><%= image.image_credit != "" ? "; Image Credit: #{image.image_credit}" : "" %></div></div>
+<div <%= size == :medium ? 'class="main_image"' : 'class="image_container"' %>><img src="<%= image.picture.url(size) %>" / ><div class="image_caption" style="max-width:#{width}px;"><%= caption %> &copy;<%= image.copyright_owner %><%= image.image_credit != "" ? "; Image Credit: #{credit}" : "" %></div></div>
 EOF
 
     template = ensure_utf8(template)
