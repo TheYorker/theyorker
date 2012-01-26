dedbca610afba3b4884e82d293f926d113b39119
David Morris <davidrowlandmorris@gmail.com>
2012-01-26 19:51:38 +0000

    Sprinkle in some more encode() calls

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -5,9 +5,14 @@ module ImagesHelper
     width = "#{image.picture.width(size)}".encode('UTF-8')
     credit = "#{image.image_credit}".encode('UTF-8')
     
-    embed = ERB.new <<EOF
+    template = <<EOF
 <div <%= size == :medium ? 'class="main_image"' : 'class="image_container"' %>><img src="<%= image.picture.url(size) %>" / ><div class="image_caption" style="max-width:#{width}px;"><%= caption %> &copy;<%= image.copyright_owner %><%= image.image_credit != "" ? "; Image Credit: #{image.image_credit}" : "" %></div></div>
 EOF
+
+    template.encode!('UTF-8')
+
+    embed = ERB.new(template)
+
     embed.result(binding).encode('UTF-8')
   end
 
