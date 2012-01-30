68d1964a90516d3d2d0b009eeeb796725788959c
David Morris <davidrowlandmorris@gmail.com>
2012-01-30 22:05:47 +0000

    Add some more validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates_presence_of :password, :on => :create
-  validates_presence_of :email, :on => :create
+
+  validates_presence_of :name
+
+  validates_presence_of :email
   validates_uniqueness_of :email
+
+  validates_presence_of :password, :on => :create
+  validates_presence_of :password_confirmation, :on => :create
+
 
   has_many :articles
   has_many :editors
