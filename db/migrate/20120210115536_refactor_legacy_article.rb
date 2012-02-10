996f02f527d08a7d5717d9d6710f41a2b87a6fd2
David Morris <davidrowlandmorris@gmail.com>
2012-02-10 12:04:54 +0000

    Refactor legacy_paths
    Remove the unnecessary fields 'potential_paths' and 'user_email' as we
    have precise section and user ids for each article.
    
    Add an index on old_id as this is what we use to find most articles.

---
+++ db/migrate/20120210115536_refactor_legacy_article.rb
@@ -0,0 +1,9 @@
+class AddUserIdToLegacyArticle < ActiveRecord::Migration
+  def change
+    change_table :legacy_articles do |t|
+      t.remove :potential_paths
+      t.remove :author_email
+      t.index :old_id
+    end
+  end
+end
