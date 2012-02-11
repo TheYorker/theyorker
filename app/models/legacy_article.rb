ddcdfc63120c0830d2b609121db810ebd187a206
David Morris <davidrowlandmorris@gmail.com>
2012-02-11 01:25:16 +0000

    Add fundamental validation to LegacyArticle model
    As all of our data is effectively pre-validated, we just need to
    deduplicate, which we can do on our old_id column

--- app/models/legacy_article.rb
+++ app/models/legacy_article.rb
@@ -3,6 +3,8 @@ class LegacyArticle < ActiveRecord::Base
   belongs_to :user
   belongs_to :section
 
+  validates :old_id, :presence => true, :uniqueness => true
+
   def date_string
     self.publish_at ? self.publish_at.to_date.to_formatted_s(:long_ordinal) : ""
   end
